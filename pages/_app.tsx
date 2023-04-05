ef0fba8235283e453347b68d30ebb21a4253c960
inannan423 <605031236@qq.com>
2023-04-05 18:06:27 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../style.css'
 import React from 'react'
 import localFont from '@next/font/local'
+// Vercel Analytics
 import { Analytics } from '@vercel/analytics/react'
 const myFont = localFont({ src: './PingFangSC.ttf' })
 
