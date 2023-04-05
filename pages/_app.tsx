d3d911a226fd2c6906c8fd4b8f8188ce6166637b
inannan423 <605031236@qq.com>
2023-04-05 17:00:09 +0800

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../style.css'
 import React from 'react'
 import localFont from '@next/font/local'
+import { Analytics } from '@vercel/analytics/react'
 const myFont = localFont({ src: './PingFangSC.ttf' })
 
 export default function App ({ Component, pageProps }): JSX.Element {
-  return <main className={myFont.className}><Component {...pageProps} /></main>
+  return <main className={myFont.className}>
+    <Component {...pageProps} />
+    <Analytics />
+  </main>
 }
