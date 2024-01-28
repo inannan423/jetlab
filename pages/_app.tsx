d0dae830785425d2cf32dfd3acc4caa4f3bda7be
jetzihan <jetzihan@outlook.com>
2024-01-28 14:28:26 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../style.css'
 import React from 'react'
-import localFont from '@next/font/local'
+// import localFont from '@next/font/local'
 // Vercel Analytics Vercel 分析
 import { Analytics } from '@vercel/analytics/react'
 // const myFont = localFont({ src: './PingFangSC.ttf' })
